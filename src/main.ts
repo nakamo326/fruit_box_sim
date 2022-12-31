23bfe4acac21c732c12ea71bd3f2704c26dc74eb
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-12-31 15:21:18 +0900

    pixi tutorial

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import Phaser from "phaser";
-import { MyScene } from "./scenes/MyScene";
+import { Assets, Application, Sprite } from 'pixi.js';
 
-const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
-  width: 800,
-  height: 600,
-  physics: {
-    default: "arcade",
-    arcade: {
-      gravity: { y: 200 },
-    },
-  },
-  scene: MyScene,
-};
-new Phaser.Game(config);
+const app = new Application();
+document.body.appendChild(app.view);
+
+const texture = await Assets.load('assets/cat.jpg');
+
+const cat = new Sprite(texture);
+
+// Setup the position of the bunny
+cat.x = app.renderer.width / 2;
+cat.y = app.renderer.height / 2;
+
+// Rotate around the center
+cat.anchor.x = 0.5;
+cat.anchor.y = 0.5;
+
+// Add the cat to the scene we are building
+app.stage.addChild(cat);
+
+// Listen for frame updates
+app.ticker.add(() => {
+  // each frame we spin the cat around a bit
+  cat.rotation += 0.01;
+});
