39c96af4920d875c1147fb1dee268c444d233492
Yasufumi Nakamoto <nakamo326@gmail.com>
2023-01-02 22:10:52 +0900

    Revert "Gameクラス"
    This reverts commit ca06a101035fa1926baadb48f6af1a4ae7098871.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,27 @@
-import { Application } from 'pixi.js';
-import { Game } from './Game';
+import {
+  Application,
+  FederatedPointerEvent,
+  Graphics,
+  ICanvas,
+  Sprite,
+} from 'pixi.js';
+import {
+  Coordinate,
+  nums,
+  GameState,
+  BOARD,
+  BOARD_STEP,
+  BOARD_START_X,
+  BOARD_START_Y,
+} from './constants';
+import { Box } from './Box';
+import { calcBoardCoordinate } from './utils';
+import { BlockFrame } from './BlockFrame';
+import { Score } from './Score';
+import { Timer } from './Timer';
+
+let lastClicked: Coordinate | null = null;
+let lastHovered: Coordinate | null = null;
 
 /*
   [x] 選択したブロックと、hoverしているブロックを囲むように枠を表示する
@@ -11,13 +33,100 @@ import { Game } from './Game';
   タイトル画面を出す
 */
 
-const main = () => {
-  // init pixi app
-  const app = new Application();
-  document.body.appendChild(app.view);
+// TODO: ファイルごとのグローバル変数をクラスに隠蔽する
 
-  const game = new Game(app);
-  game.start();
+const handleClick = (event: FederatedPointerEvent) => {
+  if (timer.isEnd) {
+    return;
+  }
+  const { x, y } = calcBoardCoordinate(event.clientX, event.clientY);
+  console.log(`(${x}, ${y})`);
+  if (!lastClicked) {
+    lastClicked = { x, y };
+    blockFrame.update(lastClicked, { x, y });
+    return;
+  }
+  const res = box.tryEraseRectangles([lastClicked, { x, y }]);
+  console.log(res);
+  if (res) {
+    box.board.forEach((line, y) => {
+      line.forEach((num, x) => {
+        if (num === 0 && !Sprites[y][x].destroyed) {
+          Sprites[y][x].destroy();
+        }
+      });
+    });
+    score.update(box.score);
+  }
+  lastClicked = null;
+  blockFrame.reset();
 };
 
-main();
+const handleOver = (event: FederatedPointerEvent) => {
+  if (timer.isEnd || !lastClicked) {
+    return;
+  }
+  const { x, y } = calcBoardCoordinate(event.clientX, event.clientY);
+  console.log(`(${x}, ${y})`);
+  if (lastHovered && x === lastHovered.x && y === lastHovered.y) {
+    return;
+  }
+  lastHovered = { x, y };
+  blockFrame.update(lastClicked, { x, y });
+};
+
+const generateBackground = (app: Application<ICanvas>) => {
+  const backGround = new Graphics();
+  backGround.beginFill(0x000000);
+  backGround.drawRect(
+    BOARD_START_X,
+    BOARD_START_Y,
+    BOARD_STEP * BOARD.X,
+    BOARD_STEP * BOARD.Y
+  );
+  backGround.endFill();
+  backGround.interactive = true;
+  backGround.on('pointerdown', handleClick);
+  backGround.on('pointerover', handleOver);
+  backGround.on('pointermove', handleOver);
+  app.stage.addChild(backGround);
+};
+
+const generateSprites = (
+  board: number[][],
+  app: Application<ICanvas>
+): Sprite[][] => {
+  const result = board.map((line, y) => {
+    return line.map((num, x) => {
+      const sprite = new Sprite(nums[num - 1]);
+      sprite.x = x * BOARD_STEP + BOARD_START_X;
+      sprite.y = y * BOARD_STEP + BOARD_START_Y;
+      app.stage.addChild(sprite);
+      sprite.interactive = true;
+      sprite.on('pointerdown', handleClick);
+      sprite.on('pointerover', handleOver);
+      return sprite;
+    });
+  });
+  return result;
+};
+
+// init pixi app
+const app = new Application();
+document.body.appendChild(app.view);
+
+const box = new Box();
+generateBackground(app);
+const Sprites = generateSprites(box.board, app);
+
+const blockFrame = new BlockFrame();
+app.stage.addChild(blockFrame.containerRef);
+
+const score = new Score();
+app.stage.addChild(score.textRef);
+
+const timer = new Timer(blockFrame.reset);
+app.stage.addChild(timer.textRef);
+
+// TODO: なんらかのクリックイベントで発火させる
+timer.setTimer();
