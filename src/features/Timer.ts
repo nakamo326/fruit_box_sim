f48972304dfa1e18b4705448a638f49a8530ec55
Yasufumi Nakamoto <nakamo326@gmail.com>
2023-01-02 23:35:21 +0900

    Timerにreset関数

--- src/features/Timer.ts
+++ src/features/Timer.ts
@@ -2,6 +2,7 @@ import { Text } from 'pixi.js';
 import { BOARD, BOARD_START_Y, BOARD_STEP } from '../constants';
 
 export class Timer {
+  private currentTimer: number | null = null;
   private timerText = new Text(`120`, {
     fontFamily: 'Arial',
     fontSize: 24,
@@ -17,14 +18,23 @@ export class Timer {
     this.timerText.y = BOARD_START_Y + BOARD_STEP * BOARD.Y + 40;
   }
 
-  setTimer() {
+  reset() {
+    this.timerText.text = '120';
+    if (this.currentTimer) {
+      clearInterval(this.currentTimer);
+    }
+  }
+
+  start() {
     const endTime = Date.now() + 120 * 1000;
 
-    const timer = setInterval(() => {
+    this.currentTimer = setInterval(() => {
       const now = Date.now();
       const rest = endTime - now;
       if (rest <= 0) {
-        clearInterval(timer);
+        if (this.currentTimer) {
+          clearInterval(this.currentTimer);
+        }
         this.isEnd = true;
         this.resetBlockFrame();
         return;
